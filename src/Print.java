37fc337ebad011c49214d19927d82c16fb45727c
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-23 10:55:25 -0300

    hmmm

---
+++ src/Print.java
@@ -0,0 +1,20 @@
+package src;
+public class Print {
+    
+    public Print () {};
+
+    private static void idk (String [] args) {
+
+        System.out.println("Hello, World!");
+
+    };
+
+    public static void main (String [] args) {
+
+        System.out.println("Another Hello, World!");
+
+        idk(args);
+
+    };
+
+};
