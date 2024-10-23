62b792784c96531dbe82da22f594e974fef2ae37
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-23 12:21:01 -0300

    string & numbers

---
+++ Numbers.java
@@ -0,0 +1,25 @@
+public class Numbers {
+
+    public static void main (String[] args) {
+
+        int value = 10;
+
+        int value2 = 20;
+
+        System.out.println(value + value2);
+
+        newNumbers(args);
+
+    };
+
+    public static void newNumbers (String[] args) {
+
+        String randomText = "go!";
+
+        String anotherText = "random bullshit";
+
+        System.out.println(anotherText + randomText);
+
+    };
+
+}
