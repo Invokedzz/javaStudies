16ff56e5a15439981dd5b120c3fd5d3426ced0f1
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-24 10:06:52 -0300

    zzz

--- src/baldFlakes/Start.java
+++ src/baldFlakes/Start.java
@@ -11,6 +11,22 @@ public class Start {
 
         System.out.println("Hello, IntelliJ");
 
+        receiveValues(args);
+
+    }
+
+    public static void receiveValues (String[] args) {
+
+        int x, y;
+
+        x = 10;
+
+        y = 30;
+
+        int addValues = x * y;
+
+        System.out.println(addValues);
+
     }
 
 }
---
+++ src/baldFlakes/Trap.java
@@ -0,0 +1,21 @@
+package src.baldFlakes;
+
+public class Trap {
+
+    public static void main (String[] args) {
+
+       float smallBase, biggerBase, area, h;
+
+       smallBase = 10.0f;
+
+       biggerBase = 30.0f;
+
+       h = 4.0f;
+
+       area = ((smallBase + biggerBase) * h) / 2;
+
+       System.out.println("The result is: " + area);
+
+    }
+
+}
