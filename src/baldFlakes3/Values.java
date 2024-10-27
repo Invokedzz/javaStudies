f806cb11262481cdfa7a51231aa4b446e65a79a9
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-27 17:26:46 -0300

    starting a new test

--- src/baldFlakes3/Values.java
+++ src/baldFlakes3/Values.java
@@ -3,25 +3,12 @@ public class Values {
     
     public static void main (String[] args) {
 
-        String firstName = "Michael";
-
-        String lastName = "Jackson";
-
-        System.out.printf("His name is: %s %s", firstName, lastName); 
-        
         receiveInfo(args);
 
     };
 
     public static void receiveInfo (String[] args) {
 
-        String personName = "Michael Jackson";
-
-        int personAge = 50;
-        
-        Float moneyReceived = 100.0f;
-
-        System.out.printf("His name is: %s, and his age is: %d, and his money is: %.2f", personName, personAge, moneyReceived);
 
     };
 
