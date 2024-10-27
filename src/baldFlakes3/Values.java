636d1996bef8d6630d5ad80b9d81ff28d65ca01b
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-27 17:34:18 -0300

    switch/case

--- src/baldFlakes3/Values.java
+++ src/baldFlakes3/Values.java
@@ -1,4 +1,7 @@
 package src.baldFlakes3;
+
+import java.util.Scanner;
+
 public class Values {
     
     public static void main (String[] args) {
@@ -9,6 +12,28 @@ public class Values {
 
     public static void receiveInfo (String[] args) {
 
+        Scanner initSc = new Scanner(System.in);
+
+        int dayInfo;
+
+        String day;
+
+        System.out.println("Insert the day of the week");
+
+        dayInfo = initSc.nextInt();
+
+        day = switch (dayInfo) {
+            case 1 -> "Dom";
+            case 2 -> "Seg";
+            case 3 -> "Ter";
+            case 4 -> "Qua";
+            case 5 -> "Qui";
+            case 6 -> "Sex";
+            case 7 -> "Sab";
+            default -> "Invalid";
+        };
+
+        System.out.println(day);
 
     };
 
