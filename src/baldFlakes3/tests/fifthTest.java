211fdf698c5c8902b9d0737a8bfc427138692cd8
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-29 11:03:03 -0300

    firstTest done

--- src/baldFlakes3/tests/fifthTest.java
+++ src/baldFlakes3/tests/fifthTest.java
@@ -1,4 +1,25 @@
 package src.baldFlakes3.tests;
 
+import java.util.Scanner;
+
 public class fifthTest {
+
+    public static void main (String[] args) {
+
+        Scanner initSc = new Scanner (System.in);
+
+        System.out.println("Insert a value (numeric)");
+
+        int x = initSc.nextInt();
+
+        for (int y = 0; y <= x; y++) {
+
+            if (y % 2 != 0) System.out.println(y);
+
+        }
+
+        System.out.println("Done!");
+
+    }
+
 }
