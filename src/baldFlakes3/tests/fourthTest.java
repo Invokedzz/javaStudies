393d729109c8d0a3372a752d0d9c250bca41a91d
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-29 14:39:11 -0300

    fourthTest done

--- src/baldFlakes3/tests/fourthTest.java
+++ src/baldFlakes3/tests/fourthTest.java
@@ -1,4 +1,37 @@
 package src.baldFlakes3.tests;
 
+import java.util.Scanner;
+
 public class fourthTest {
+
+    public static void main (String[] args) {
+
+        Scanner initSc = new Scanner(System.in);
+
+        System.out.println("Insert the sequence");
+
+        int receiveValue = initSc.nextInt();
+
+        for (int q = 0; q < receiveValue; q++) {
+
+            System.out.println("Insert (1), (2), (3)");
+
+            double firstValue = initSc.nextDouble();
+
+            double secondValue = initSc.nextDouble();
+
+            double thirdValue = initSc.nextDouble();
+
+            double addReceivedValues = (firstValue * 2.0 + secondValue * 3.0 + thirdValue * 5.0);
+
+            double resultTotal = addReceivedValues / 10.0;
+
+            System.out.println(resultTotal);
+
+        }
+
+        System.out.println("Done!");
+
+    }
+
 }
