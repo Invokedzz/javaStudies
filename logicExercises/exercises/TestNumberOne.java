6eaa38ab35daee00c435a3b9820acdb500369224
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-31 14:47:57 -0300

    someExercises for an exam

---
+++ logicExercises/exercises/TestNumberOne.java
@@ -0,0 +1,27 @@
+package logicExercises.exercises;
+
+import java.util.Scanner;
+
+public class TestNumberOne {
+
+    public static void main (String[] args) {
+
+        Scanner initSc = new Scanner (System.in);
+
+        double receiveKm = initSc.nextDouble();
+
+        if (receiveKm > 80) {
+
+            System.out.println("Foi m!");
+
+            double kmReserved = receiveKm - 80.0;
+
+            double totalResult = kmReserved * 5.0;
+
+            System.out.println(totalResult);
+
+        }
+
+    }
+
+}
