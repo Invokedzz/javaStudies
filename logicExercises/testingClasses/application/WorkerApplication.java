45a0565539a8d11592659950921ec2a8b4ea1f87
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-03 19:36:51 -0300

    adding functions

---
+++ logicExercises/testingClasses/application/WorkerApplication.java
@@ -0,0 +1,21 @@
+package logicExercises.testingClasses.application;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class WorkerApplication {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+    }
+
+    public static void insertingTheProperties () {
+
+        Scanner initSc = new Scanner (System.in);
+
+    }
+
+}
---
+++ logicExercises/testingClasses/entities/WorkerEntity.java
@@ -0,0 +1,26 @@
+package logicExercises.testingClasses.entities;
+
+public class WorkerEntity {
+
+    public String Name;
+
+    public double GrossSalary;
+
+    public double Tax;
+
+    public String netInformation () {
+
+        return Name + " " + GrossSalary + " " + Tax;
+
+    }
+
+    public void netSalary (double por) {
+
+        GrossSalary += GrossSalary * (por / 100.0);
+
+    }
+
+
+
+
+}
