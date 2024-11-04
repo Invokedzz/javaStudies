637cba02266747a884a4b812f11e4d10e92ceda6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-04 12:10:19 -0300

    creating our entities

--- src/staticMembers/application/DollarApplication.java
+++ src/staticMembers/application/DollarApplication.java
@@ -1,4 +1,25 @@
 package src.staticMembers.application;
 
+import java.util.Scanner;
+
+import java.util.Locale;
+
+import src.staticMembers.entities.DollarEntity;
+
 public class DollarApplication {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+        receiveDollar();
+
+    }
+
+    private static void receiveDollar () {
+
+        DollarEntity receptor = new DollarEntity();
+
+    }
+
 }
--- src/staticMembers/entities/DollarEntity.java
+++ src/staticMembers/entities/DollarEntity.java
@@ -1,4 +1,21 @@
 package src.staticMembers.entities;
 
 public class DollarEntity {
+
+    public static double dollarCurrentValue;
+
+    public static double productBoughtByUser;
+
+    public static double receiveBothValues;
+
+    public static double returnValues (double dollarCurrentValue, double productBoughtByUser) {
+        receiveBothValues = dollarCurrentValue * productBoughtByUser;
+        return receiveBothValues;
+    }
+
+    public static double taxesUponTheValue () {
+        receiveBothValues += receiveBothValues * 0.06;
+        return receiveBothValues;
+    }
+
 }
