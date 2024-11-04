fa90284f9a4a9b8f391ed56073b0419d312e6757
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-04 10:53:17 -0300

    initing entities + its former functions

---
+++ src/staticMembers/application/SecondTestApplication.java
@@ -0,0 +1,15 @@
+package src.staticMembers.application;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class SecondTestApplication {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+    }
+
+}
---
+++ src/staticMembers/entities/SecondTestEntity.java
@@ -0,0 +1,19 @@
+package src.staticMembers.entities;
+
+public class SecondTestEntity {
+
+    public double radius;
+
+    public static double PI = 3.14;
+
+    public static double circumference (double radius) {
+
+        
+
+    }
+
+    public static double volume (double radius) {
+
+    }
+
+}
