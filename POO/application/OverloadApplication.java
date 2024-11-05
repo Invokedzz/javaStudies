780c80224cedea6ff0578df41a08ed35e35d24b6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-05 13:05:55 -0300

    overload !!!

--- POO/application/OverloadApplication.java
+++ POO/application/OverloadApplication.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 
 import POO.entity.OverloadEntity;
 
+import POO.entity.FunctionsComments;
+
 public class OverloadApplication {
 
     public static void main (String[] args) {
@@ -19,6 +21,32 @@ public class OverloadApplication {
     public static void ourApplication () {
 
         Scanner initSc = new Scanner (System.in);
+
+        FunctionsComments init = new FunctionsComments();
+
+        init.insertYourName();
+
+        OverloadEntity.name = initSc.next();
+
+        init.insertTotalObtained();
+
+        OverloadEntity.receivedValue = initSc.nextDouble();
+
+        init.insertYourCurrentValue();
+
+        OverloadEntity.totalValueObtained = initSc.nextDouble();
+
+        receiveInfo(OverloadEntity.name, OverloadEntity.receivedValue, OverloadEntity.totalValueObtained);
+
+    }
+
+    public static void receiveInfo (String name, double receivedValue, double totalValueObtained) {
+
+        OverloadEntity product = new OverloadEntity();
+
+        product.constructorOverloadEntity(name, receivedValue, totalValueObtained);
+
+        System.out.println(product.addedValuesTotal(receivedValue, totalValueObtained));
 
     }
 
---
+++ POO/entity/FunctionsComments.java
@@ -0,0 +1,23 @@
+package POO.entity;
+
+public class FunctionsComments {
+
+    public void insertYourName () {
+
+        System.out.println("Enter your name, user!\n");
+
+    }
+
+    public void insertYourCurrentValue () {
+
+        System.out.println("Enter your current value\n");
+
+    }
+
+    public void insertTotalObtained () {
+
+        System.out.println("Enter how much money you will earn this month\n");
+
+    }
+
+}
