9a9c6de0c82480da0dafe1cf40c3c171873e8f08
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-05 10:24:24 -0300

    constructor = construct ?

---
+++ POO/application/Construct.java
@@ -0,0 +1,25 @@
+package POO.application;
+
+import POO.entity.Destroy;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class Construct {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.GERMAN);
+
+        thinkAboutIt();
+
+    }
+
+    public static void thinkAboutIt () {
+
+        Scanner initScanner = new Scanner (System.in);
+
+    }
+
+}
---
+++ POO/entity/Destroy.java
@@ -0,0 +1,21 @@
+package POO.entity;
+
+public class Destroy {
+
+    public String receiveName;
+
+    public double totalValue;
+
+    public double addToValue;
+
+    public Destroy (String receiveName, double totalValue, double addToValue) {
+
+        this.receiveName = receiveName;
+
+        this.addToValue = addToValue;
+
+        this.totalValue = totalValue;
+
+    }
+
+}
