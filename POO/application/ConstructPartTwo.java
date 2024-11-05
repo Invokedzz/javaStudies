48f3f9c2e27219b76d21564005093daaeb48d09f
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-05 12:32:14 -0300

    pt2

--- POO/application/ConstructPartTwo.java
+++ POO/application/ConstructPartTwo.java
@@ -10,6 +10,28 @@ public class ConstructPartTwo {
 
     public static void main (String[] args) {
 
+        Locale.setDefault(Locale.ITALIAN);
+
+        receiveBothValues();
+
+    }
+
+    public static void receiveBothValues () {
+
+        Scanner initSc = new Scanner (System.in);
+
+        String receiveName = initSc.next();
+
+        double totalValue = initSc.nextDouble();
+
+        double addValueTo = initSc.nextDouble();
+
+        DestroyPartTwo productConstructor = new DestroyPartTwo(receiveName, totalValue, addValueTo);
+
+        System.out.println(DestroyPartTwo.returnFinalValues(totalValue, addValueTo));
+
+        System.out.println(productConstructor);
+
     }
 
 }
