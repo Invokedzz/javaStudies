9e4070b7c4a960a8cda2863d33722d8877897344
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-05 10:57:31 -0300

    init

---
+++ POO/application/ConstructPartTwo.java
@@ -0,0 +1,15 @@
+package POO.application;
+
+import POO.entity.DestroyPartTwo;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class ConstructPartTwo {
+
+    public static void main (String[] args) {
+
+    }
+
+}
---
+++ POO/entity/DestroyPartTwo.java
@@ -0,0 +1,11 @@
+package POO.entity;
+
+public class DestroyPartTwo {
+
+    public static String receiveFullName;
+
+    public static double totalValue;
+
+    public static double addValueTo;
+
+}
