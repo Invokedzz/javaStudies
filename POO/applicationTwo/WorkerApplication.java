9dba2005d18714beb4d7b4df158e3a29b635e2a4
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-06 10:45:52 -0300

    one more exercise done

---
+++ POO/applicationTwo/WorkerApplication.java
@@ -0,0 +1,37 @@
+package POO.applicationTwo;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+import POO.entitiesTwo.WorkerEntity;
+
+public class WorkerApplication {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+        receiveWorkerMainAttributes();
+
+    }
+
+    public static void receiveWorkerMainAttributes () {
+
+        Scanner initSc = new Scanner (System.in);
+
+        WorkerEntity product = new WorkerEntity();
+
+        product.workerName = initSc.next();
+
+        product.workerSalary = initSc.nextDouble();
+
+        product.checkSalary = initSc.nextDouble();
+
+        System.out.println(product.returnWorkerName());
+
+        System.out.println(product.reviewSalary(product.checkSalary));
+
+    }
+
+}
