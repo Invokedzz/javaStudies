0a7ead9426225a8f9d7abd9eabe7e72c2e8b2dd7
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-07 11:01:57 -0300

    creating classes

---
+++ POO/ApplicationThree/ApplicationThree.java
@@ -0,0 +1,21 @@
+package POO.ApplicationThree;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+import POO.ApplicationThree.ApplicationThree;
+
+public class ApplicationThree {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.GERMAN);
+
+    }
+
+    public static void carPatterns () {
+
+    }
+
+}
---
+++ POO/EntitiesThree/EntitiesThree.java
@@ -0,0 +1,15 @@
+package POO.EntitiesThree;
+
+public class EntitiesThree {
+
+    private int carID;
+
+    private String carBrand;
+
+    private String carColor;
+
+    private double totalValue;
+
+    private double distanceTraveled;
+
+}
