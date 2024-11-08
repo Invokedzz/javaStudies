623a6ecfcde339cf4f475b0c206a929789c8596f
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-08 14:48:27 -0300

    manipulating some arrays

---
+++ arrays/FirstArray.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class FirstArray {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+        Scanner initSc = new Scanner (System.in);
+
+        int randomStartValue = initSc.nextInt();
+
+        double [] vector = new double[randomStartValue];
+
+        for (int a = 0; a < randomStartValue; a++) {
+
+            vector[a] = initSc.nextDouble();
+
+        }
+
+        double sum = 0.0;
+
+        for (int i = 0; i < randomStartValue; i++) {
+            sum += vector[i];
+        }
+
+        double trueValue = sum / randomStartValue;
+
+        System.out.println(trueValue);
+
+        initSc.close();
+
+    }
+
+}
