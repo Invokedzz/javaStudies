669fbf7aa0ed9a3baf6b5cb962322df76e9c261c
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-09 11:19:16 -0300

    SecondExercise

---
+++ arrays/SecondExercise.java
@@ -0,0 +1,55 @@
+package arrays;
+
+import java.util.Scanner;
+
+import java.util.Locale;
+
+public class SecondExercise {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.KOREA);
+
+        receiveArrayValues();
+
+    }
+
+    public static void receiveArrayValues () {
+
+        Scanner initSc = new Scanner (System.in);
+
+        int receivedValue = initSc.nextInt();
+
+        double [] vector = new double[receivedValue];
+
+        for (int p = 0; p < receivedValue; p ++) {
+
+            System.out.println("Enter the values: ");
+
+            vector[p] = initSc.nextDouble();
+
+        }
+
+        double receivedNumbers = 0.0;
+
+        double showNumbers;
+
+        for (int a = 0; a < receivedValue; a ++) {
+
+            showNumbers = vector[a];
+
+            receivedNumbers += vector[a];
+
+            System.out.println("Total: " + receivedNumbers);
+
+            System.out.printf("Values = %s", showNumbers);
+
+        }
+
+        double m = receivedNumbers / receivedValue;
+
+        System.out.println(m);
+
+    }
+
+}
