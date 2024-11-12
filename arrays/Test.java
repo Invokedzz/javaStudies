261b4bc7e9eb9faf2c93d061781287bc6fc13197
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-12 15:08:08 -0300

    zzzzz

---
+++ arrays/Test.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Test {
+
+    public static void main (String[] args) {
+
+        int x = 0;
+
+        do {
+
+            System.out.println(x);
+
+            x += 1;
+
+        } while(x <= 10);
+
+    }
+
+}
