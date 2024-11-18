459c73cada2fd4d65d2f756270c7bc26e55a9b18
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-18 11:15:32 -0300

    one more exercise done

--- review/otherFiles/StoreApp.java
+++ review/otherFiles/StoreApp.java
@@ -18,6 +18,40 @@ public class StoreApp {
 
     private static void ourOwnStoreApp () {
 
+        Scanner initSc = new Scanner(System.in);
+
+        int arrayParameter = initSc.nextInt();
+
+        String [] productName = new String[arrayParameter];
+
+        double [] productPrice = new double[arrayParameter];
+
+        StoreEntity productMainClass = new StoreEntity(productName, productPrice);
+
+        for (int i = 0; i < arrayParameter; i++) {
+
+            System.out.println("Enter the product name: ");
+
+            productName[i] = initSc.next();
+
+            System.out.println("Enter the product value: ");
+
+            productPrice[i] = initSc.nextDouble();
+
+        }
+
+        double average = 0.0;
+
+        for (int y = 0; y < arrayParameter; y++) {
+
+             average = productMainClass.valuesAddition(productPrice, y);
+
+        }
+
+        double totalResult = average / arrayParameter;
+
+        System.out.println("Total: " + totalResult);
+
     }
 
 }
--- review/otherFiles/StoreEntity.java
+++ review/otherFiles/StoreEntity.java
@@ -1,16 +1,28 @@
 package review.otherFiles;
 
+import java.util.Arrays;
+
 public class StoreEntity {
 
     private String productName;
 
-    private double productPrice;
+    private double [] productPrice;
 
-    public StoreEntity (String productName, double productPrice) {
+    private double totalResult;
 
-        this.productName = productName;
+    public StoreEntity (String [] productName, double [] productPrice) {
+
+        this.productName = Arrays.toString(productName);
 
         this.productPrice = productPrice;
+
+    }
+
+    public double valuesAddition (double [] productPrice, int value) {
+
+        totalResult += productPrice[value];
+
+        return totalResult;
 
     }
 
@@ -24,7 +36,7 @@ public class StoreEntity {
         return productName;
     }
 
-    public double getProductPrice () {
+    public double [] getProductPrice () {
         return productPrice;
     }
 
