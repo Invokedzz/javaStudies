9376a4e63a28aa60615c11aaacbbfdef600a9c78
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-19 18:02:49 -0300

    commit

--- arrays/someStuff/StringListLearnAgain.java
+++ arrays/someStuff/StringListLearnAgain.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-// import java.util.stream.Collectors;
-
 public class StringListLearnAgain {
 
     public static void main (String[] args) {
@@ -18,21 +16,38 @@ public class StringListLearnAgain {
 
         List <String> listElements = new ArrayList<>();
 
+        List <String> anotherListOfElements = new ArrayList<>();
+
         listElements.add("Paul");
         listElements.add("Tobias");
         listElements.add("Chico");
         listElements.add("Paolo");
         listElements.add("Honey");
 
+        anotherListOfElements.add("Thom");
+        anotherListOfElements.add("Tom");
+        anotherListOfElements.add("Yorke");
+        anotherListOfElements.add("Chester");
+
         String name = listElements.stream()
                 .filter(value -> value.charAt(0) == 'P')
                 .findFirst().orElse(null);
+
+        String newName = anotherListOfElements.stream()
+                .filter(newValue -> newValue.charAt(0) == 'T').toList().toString();
 
         if (name != null) {
 
             String valueObtained = name.toUpperCase();
 
             System.out.println(valueObtained);
+
+        }
+
+        if (newName != null) {
+
+            String anotherValueObtained = newName.toUpperCase();
+            System.out.println(anotherValueObtained);
 
         }
 
