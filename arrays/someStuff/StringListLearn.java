dc1c78cdd3eba212109fbfb069a82518aef057ff
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-19 14:48:48 -0300

    having some fun

--- arrays/someStuff/StringListLearn.java
+++ arrays/someStuff/StringListLearn.java
@@ -20,6 +20,43 @@ public class StringListLearn {
 
         List <String> ourList = new ArrayList<>();
 
+        ourList.add("Honey - Pablo");
+
+        ourList.add("Radiohead");
+
+        ourList.add(0, "Thom Yorke");
+
+        ourList.add(1, "No Surprises");
+
+        ourList.remove(2);
+
+        System.out.println(ourList);
+
+        for (String obj: ourList) {
+            System.out.println(obj);
+        }
+
+        System.out.println("--------------------------------");
+
+        List <String> anotherList = new ArrayList<>();
+
+        anotherList.add("Adele");
+        anotherList.add("Amy Lee");
+        anotherList.add("Avril Lavigne");
+        anotherList.add("Eminem");
+        anotherList.add("Chester Bennington");
+
+        anotherList.removeIf(x -> x.charAt(1) == 'v');
+
+        System.out.println(anotherList);
+
+        System.out.println(anotherList.indexOf("Amy Lee"));
+
+        for (String element: anotherList) {
+            System.out.println(element);
+        }
+
+
     }
 
 }
