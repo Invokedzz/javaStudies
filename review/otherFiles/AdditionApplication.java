24fb53664a9684be9536b4aa5cf78882e1ef9458
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-19 09:18:51 -0300

    entity created

--- review/otherFiles/AdditionApplication.java
+++ review/otherFiles/AdditionApplication.java
@@ -1,4 +1,21 @@
 package review.otherFiles;
 
+import review.otherFiles.AdditionEntity;
+
+import java.util.Scanner;
+
 public class AdditionApplication {
+
+    public static void main (String[] args) {
+
+        startMainProject();
+
+    }
+
+    private static void startMainProject () {
+
+        Scanner initSc = new Scanner (System.in);
+
+    }
+
 }
--- review/otherFiles/AdditionEntity.java
+++ review/otherFiles/AdditionEntity.java
@@ -1,4 +1,26 @@
 package review.otherFiles;
 
 public class AdditionEntity {
+
+    private Double valueSentByUser;
+
+    private Double totalResult;
+
+    public AdditionEntity (Double valueSentByUser) {
+        this.valueSentByUser = valueSentByUser;
+    }
+
+    public Double addValuesToVariable (Double valueSentByUser) {
+         totalResult += valueSentByUser;
+         return totalResult;
+    }
+
+    public Double getValueSentByUser () {
+        return valueSentByUser;
+    }
+
+    public void setValueSentByUser (Double valueSentByUser) {
+        this.valueSentByUser = valueSentByUser;
+    }
+
 }
