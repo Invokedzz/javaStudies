d3a0db65f0058c92efed9544245b4f2ba8a815a9
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-20 16:19:02 -0300

    starting our mainApplication

--- superMarket/application/MainApplication.java
+++ superMarket/application/MainApplication.java
@@ -1,4 +1,41 @@
 package superMarket.application;
 
+import java.util.Scanner;
+
+import superMarket.entities.MarketEntity;
+
+import java.util.Locale;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
 public class MainApplication {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.CHINESE);
+
+        ourMarketPlace();
+
+    }
+
+    private static void ourMarketPlace () {
+
+        Scanner initSc = new Scanner(System.in);
+
+        List <MarketEntity> list = new ArrayList<>();
+
+        System.out.println("Enter the products qty: ");
+
+        int productsQty = initSc.nextInt();
+
+        Integer [] controlLoop = new Integer[productsQty];
+
+        for (int i = 0; i < controlLoop.length; i++) {
+
+        }
+
+    }
+
 }
--- superMarket/entities/MarketEntity.java
+++ superMarket/entities/MarketEntity.java
@@ -1,4 +1,32 @@
 package superMarket.entities;
 
 public class MarketEntity {
+
+    private final String productName;
+
+    private final Integer productId;
+
+    private final Double productPrice;
+
+    public MarketEntity (String productName, Integer productId, Double productPrice) {
+
+        this.productName = productName;
+
+        this.productId = productId;
+
+        this.productPrice = productPrice;
+
+    }
+
+    @Override
+    public String toString () {
+        return productName
+                + " " + productId
+                + " " + productPrice;
+    }
+
+    public Integer getProductId () {
+        return productId;
+    }
+
 }
