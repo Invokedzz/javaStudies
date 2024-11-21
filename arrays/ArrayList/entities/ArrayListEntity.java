19bc2c1013ce9ba5d1d677411d6630f52a71b3ac
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-21 11:30:58 -0300

    creating our entity

--- arrays/ArrayList/entities/ArrayListEntity.java
+++ arrays/ArrayList/entities/ArrayListEntity.java
@@ -1,4 +1,27 @@
 package arrays.ArrayList.entities;
 
+import java.util.ArrayList;
+
 public class ArrayListEntity {
+
+    private final String gameName;
+
+    private final Integer gameId;
+
+    public ArrayListEntity (String gameName, Integer gameId) {
+
+        this.gameName = gameName;
+        this.gameId = gameId;
+
+    }
+
+    @Override
+    public String toString () {
+        return gameName + " " + gameId;
+    }
+
+    public Integer getGameId () {
+        return gameId;
+    }
+
 }
