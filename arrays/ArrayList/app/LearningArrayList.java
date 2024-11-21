6498e2c03bed326121bd2a9e3cc4cb5321790b6b
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-21 11:16:15 -0300

    man i love arrays

--- arrays/ArrayList/app/LearningArrayList.java
+++ arrays/ArrayList/app/LearningArrayList.java
@@ -26,8 +26,18 @@ public class LearningArrayList {
         list.add("Mustang");
         list.add("Volvo");
 
+        System.out.println(list);
+
+        System.out.println("-----------------------------");
+
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i)); // This method runs through our ArrayList;
+        }
+
+        System.out.println("-----------------------------");
+
+        for (String elements: list) {
+            System.out.println(elements); // Using the loop for each to iterate over each element;
         }
 
     }
