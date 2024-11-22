becc389710dbc48f71074fea9a1c256b0489ec71
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-22 11:51:09 -0300

    zzz

--- arrays/ArrayList/app/ExeArrayList.java
+++ arrays/ArrayList/app/ExeArrayList.java
@@ -8,9 +8,45 @@ import java.util.ArrayList;
 
 import java.util.Scanner;
 
+import java.util.Locale;
+
 public class ExeArrayList {
 
     public static void main (String[] args) {
+
+        Locale.setDefault(Locale.ITALIAN);
+
+        reviewOfArrayList();
+
+    }
+
+    private static void reviewOfArrayList () {
+
+        Scanner init = new Scanner(System.in);
+
+        List <ExeArrayListEntity> list = new ArrayList<>();
+
+        int controlValue = init.nextInt();
+
+        for (int i = 0; i < controlValue; i++) {
+
+            System.out.println("Enter your name: ");
+
+            String userName = init.next();
+
+            System.out.println("Enter your age: ");
+
+            Integer userAge = init.nextInt();
+
+            ExeArrayListEntity user = new ExeArrayListEntity(userName, userAge);
+
+            list.add(user);
+
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+
+        }
 
     }
 
---
+++ arrays/ArrayList/app/NumericalArrayList.java
@@ -0,0 +1,15 @@
+package arrays.ArrayList.app;
+
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class NumericalArrayList {
+
+    public static void main (String[] args) {
+
+    }
+
+}
