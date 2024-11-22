eecd75896f613f965097b6e4a5b2f676b8891e14
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-22 11:38:49 -0300

    Update MatrixExercise.java

--- arrays/matrix/MatrixExercise.java
+++ arrays/matrix/MatrixExercise.java
@@ -23,11 +23,12 @@ public class MatrixExercise {
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
+            for (int i = 0; i < matrix.length; i++) {
 
-            for (int j = 0; j < matrix[i].length; j++) {
+                for (int j = 0; j < matrix[i].length; j++) {
 
-                System.out.println(matrix[i][j]);
+
+                    System.out.println(matrix[i][j]);
 
             }
         }
