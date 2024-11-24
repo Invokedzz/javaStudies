7e6dc4dc4539c17d5bd9a4e1037b0423e4208e94
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-24 17:15:18 -0300

    parsing

--- dateTime/SecondDateTime.java
+++ dateTime/SecondDateTime.java
@@ -16,11 +16,19 @@ public class SecondDateTime {
 
         LocalDateTime thirdElement = LocalDateTime.now();
 
+        LocalTime fourthElement = LocalTime.parse("10:15:30"); // LocalTime parse format -> 20:03:22 (for example)
+
+        LocalDate fifthElement = LocalDate.parse("2005-08-10");
+
         System.out.println(firstElement);
 
         System.out.println(secondElement);
 
         System.out.println(thirdElement);
+
+        System.out.println(fourthElement);
+
+        System.out.println(fifthElement);
 
 
     }
