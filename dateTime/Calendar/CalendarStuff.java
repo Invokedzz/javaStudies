903420f8ce1f7743894910fa48fa6c04d7b5c200
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-26 12:13:47 -0300

    there you go

---
+++ dateTime/Calendar/CalendarStuff.java
@@ -0,0 +1,38 @@
+package dateTime.Calendar;
+
+import java.time.Instant;
+import java.util.Calendar;
+
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+
+public class CalendarStuff {
+
+    public static void main (String[] args) {
+
+        creatingSomeCalendarThings();
+
+    }
+
+    private static void creatingSomeCalendarThings () {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        Date getDate = Date.from(Instant.parse("2023-09-10T10:10:40Z"));
+
+        System.out.println(dateFormat.format(getDate));
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(getDate);
+
+        calendar.add(calendar.HOUR_OF_DAY, 4);
+
+        getDate = calendar.getTime();
+
+        System.out.println(dateFormat.format(getDate));
+
+    }
+
+}
