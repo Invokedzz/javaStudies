e5b285f3fe5ca51bfb77a3d65d8ca265814b35ed
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-28 11:10:05 -0300

    switch for orders

--- localstore/app/MainStore.java
+++ localstore/app/MainStore.java
@@ -30,6 +30,54 @@ public class MainStore {
 
     }
 
+    private static StoreOrders orders (char orderStatus) {
+
+        return switch (orderStatus) {
+
+            case 'p' | 'P' -> {
+
+                System.out.println("Pending payment. \n");
+
+                yield StoreOrders.PENDING_PAYMENT;
+
+            }
+
+            case 'a' | 'A' -> {
+
+                System.out.println("Payment approved! \n");
+
+                yield StoreOrders.PAYMENT_APPROVED;
+
+            }
+
+            case 's' | 'S' -> {
+
+                System.out.println("Product shipped. \n");
+
+                yield StoreOrders.PRODUCT_SHIPPED;
+
+            }
+
+            case 'd' | 'D' -> {
+
+                System.out.println("Product delivered. \n");
+
+                yield StoreOrders.DELIVERED;
+
+            }
+
+            default -> {
+
+                System.out.println("Product was not found in the database");
+
+                yield StoreOrders.NOT_FOUND;
+
+            }
+
+        };
+
+    }
+
     private static void chooseOptions (Scanner init, char element) {
 
         switch (element) {
