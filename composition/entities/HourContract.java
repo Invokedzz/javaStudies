b5b3fb926d3b2ae13b90a559f29d2e55c55fe478
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-29 13:54:18 -0300

    hourcontract setup

--- composition/entities/HourContract.java
+++ composition/entities/HourContract.java
@@ -1,4 +1,47 @@
 package composition.entities;
 
+import java.time.LocalDate;
+
 public class HourContract {
+
+    private LocalDate date;
+
+    private Double valuePerHour;
+
+    private Integer hours;
+
+    public HourContract (LocalDate date, Double valuePerHour, Integer hours) {
+
+        this.date = date;
+
+        this.valuePerHour = valuePerHour;
+
+        this.hours = hours;
+
+    }
+
+    public Integer getHours () {
+        return hours;
+    }
+
+    public Double getValuePerHour () {
+        return valuePerHour;
+    }
+
+    public LocalDate getDate () {
+        return date;
+    }
+
+    public void setDate (LocalDate date) {
+        this.date = date;
+    }
+
+    public void setHours (Integer hours) {
+        this.hours = hours;
+    }
+
+    public void setValuePerHour (Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
 }
