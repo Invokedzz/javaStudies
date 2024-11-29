0575df156ccedf05aca3b22dc1dfbfa23b8dbcb8
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-29 12:23:03 -0300

    department & worker level

--- composition/entities/Department.java
+++ composition/entities/Department.java
@@ -1,4 +1,17 @@
 package composition.entities;
 
 public class Department {
+
+    private String name;
+
+    public Department () {}
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
 }
--- composition/entities/enums/WorkerLevel.java
+++ composition/entities/enums/WorkerLevel.java
@@ -1,4 +1,11 @@
 package composition.entities.enums;
 
 public enum WorkerLevel {
+
+    JUNIOR,
+
+    MID_LEVEL,
+
+    SENIOR,
+
 }
