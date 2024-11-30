92453e3b00810c562bbd91296ed9d075380aabdb
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-30 15:22:24 -0300

    hmmmm

--- Client/entities/Product.java
+++ Client/entities/Product.java
@@ -1,4 +1,25 @@
 package Client.entities;
 
 public class Product {
+
+    private String productName;
+
+    private double productPrice;
+
+    public Product (String productName, double productPrice) {
+
+        this.productName = productName;
+
+        this.productPrice = productPrice;
+
+    }
+
+    public String getProductName () {
+        return productName;
+    }
+
+    public double getProductPrice () {
+        return productPrice;
+    }
+
 }
