a4b3792f3841d8b51a66c9077a8ccb748c1ceea7
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-11-30 15:04:30 -0300

    starting a new project

---
+++ Client/app/MainApp.java
@@ -0,0 +1,4 @@
+package Client.app;
+
+public class MainApp {
+}
---
+++ Client/entities/enums/OrderStatus.java
@@ -0,0 +1,13 @@
+package Client.entities.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+
+    PROCESSING,
+
+    SHIPPED,
+
+    DELIVERED,
+
+}
