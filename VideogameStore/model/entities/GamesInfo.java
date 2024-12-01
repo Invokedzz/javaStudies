ac868fd9a968f33fb8281faed87ec6b08888417a
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-01 19:25:57 -0300

    Basic info class + enums

--- VideogameStore/model/entities/GamesInfo.java
+++ VideogameStore/model/entities/GamesInfo.java
@@ -1,4 +1,40 @@
 package VideogameStore.model.entities;
 
+import VideogameStore.model.enums.ProductsAvailability;
+
 public class GamesInfo {
+
+    private final String gameName;
+
+    private Double gamePrice;
+
+    private final ProductsAvailability availability;
+
+    public GamesInfo (String gameName, Double gamePrice, ProductsAvailability availability) {
+
+        this.gameName = gameName;
+
+        this.gamePrice = gamePrice;
+
+        this.availability = availability;
+
+    }
+
+    public double verifyAvailability (ProductsAvailability stats) {
+
+        if (stats.equals(ProductsAvailability.OUT_OF_STOCK)) return gamePrice = 0.0;
+
+        return gamePrice;
+
+    }
+
+    @Override
+    public String toString () {
+
+        return "Game: " + gameName +
+                " Price: " + gamePrice
+                + " Stats: " + availability;
+
+    }
+
 }
---
+++ VideogameStore/model/enums/ProductsAvailability.java
@@ -0,0 +1,9 @@
+package VideogameStore.model.enums;
+
+public enum ProductsAvailability {
+
+    OUT_OF_STOCK,
+
+    AVAILABLE,
+
+}
