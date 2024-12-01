a9f3a27ca4d5d0dea5226ced6563c64d2cc05ee6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-01 19:30:42 -0300

    gamesoverlook

--- VideogameStore/model/entities/GamesOverlook.java
+++ VideogameStore/model/entities/GamesOverlook.java
@@ -1,4 +1,34 @@
 package VideogameStore.model.entities;
 
+import VideogameStore.model.entities.GamesInfo;
+
+import java.time.LocalDate;
+
 public class GamesOverlook {
+
+    private final LocalDate releaseGameDate;
+
+    private final String gameGenre;
+
+    private final GamesInfo gamesInfo;
+
+    public GamesOverlook (LocalDate releaseGameDate, String gameGenre, GamesInfo gamesInfo) {
+
+        this.releaseGameDate = releaseGameDate;
+
+        this.gameGenre = gameGenre;
+
+        this.gamesInfo = gamesInfo;
+
+    }
+
+    @Override
+    public String toString () {
+
+        return "Release: " + releaseGameDate
+                + " Genre: " + gameGenre
+                + " Info: " + gamesInfo;
+
+    }
+
 }
