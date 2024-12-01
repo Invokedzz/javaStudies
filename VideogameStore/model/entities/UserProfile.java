f93f1fe5a0948ab668f006ccc6bfcdccd2d6eab3
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-01 19:01:00 -0300

    creating UserProfile class

--- VideogameStore/model/entities/UserProfile.java
+++ VideogameStore/model/entities/UserProfile.java
@@ -1,4 +1,43 @@
 package VideogameStore.model.entities;
 
+import VideogameStore.model.enums.UserStatus;
+
 public class UserProfile {
+
+    private final String firstName;
+
+    private final String lastName;
+
+    private final String password;
+
+    private final UserStatus status;
+
+    public UserProfile (String firstName, String lastName, String password, UserStatus status) {
+
+        this.firstName = firstName;
+
+        this.lastName = lastName;
+
+        this.password = password;
+
+        this.status = status;
+
+    }
+
+    public String getPassword () {
+
+        return password;
+
+    }
+
+    @Override
+    public String toString () {
+
+        return firstName
+                + " " + lastName
+                + " " + password +
+                " " + status;
+
+    }
+
 }
