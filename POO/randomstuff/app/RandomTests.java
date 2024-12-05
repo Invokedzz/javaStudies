b74924fdc4867cd7d7be7942178cc47c3b6dc866
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-05 15:45:47 -0300

    .

--- POO/randomstuff/app/RandomTests.java
+++ POO/randomstuff/app/RandomTests.java
@@ -1,4 +1,34 @@
 package POO.randomstuff.app;
 
+import POO.randomstuff.entities.RandomEntitiesForRandomTests;
+
+import javax.swing.JOptionPane;
+
 public class RandomTests {
+
+    public static void main ( String[] args ) {
+
+        mainFunction();
+
+    }
+
+    private static void mainFunction () {
+
+        JOptionPane.showMessageDialog(null, "Welcome!");
+
+        JOptionPane.showMessageDialog(null, "How are you?");
+
+        JOptionPane.showOptionDialog(
+                null
+                , "Question?"        // Message
+                , "Title"               // Title
+                , JOptionPane.YES_NO_OPTION
+                , JOptionPane.PLAIN_MESSAGE
+                , null // Icon
+                , null // Values for buttons
+                , "Random button"    // Label default button
+        );
+
+    }
+
 }
--- POO/randomstuff/entities/RandomEntitiesForRandomTests.java
+++ POO/randomstuff/entities/RandomEntitiesForRandomTests.java
@@ -1,4 +1,31 @@
 package POO.randomstuff.entities;
 
 public class RandomEntitiesForRandomTests {
+
+    private String writeSomethingHere;
+
+    public RandomEntitiesForRandomTests (String writeSomethingHere) {
+
+        this.writeSomethingHere = writeSomethingHere;
+
+    }
+
+    public String getWriteSomethingHere () {
+
+        return writeSomethingHere;
+
+    }
+
+    public void setWriteSomethingHere (String writeSomethingHere) {
+
+        this.writeSomethingHere = writeSomethingHere;
+
+    }
+
+    public String returnSomeKindOfDumbText (String writeSomethingHere) {
+
+        return "Dumb shit: " + writeSomethingHere;
+
+    }
+
 }
