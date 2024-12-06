7aad24cf6e179c6ecd141c0590b90dd23e438068
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-06 13:59:41 -0300

    :D

--- POO/somexercises/app/MainInvoice.java
+++ POO/somexercises/app/MainInvoice.java
@@ -6,7 +6,23 @@ public class MainInvoice {
 
     public static void main ( String[] args ) {
 
+        mainOperation();
 
+    }
+
+    private static void mainOperation () {
+
+        EntityInvoice invoice = new EntityInvoice( "12", "hello!", 10, 20 );
+
+        double getAmount = invoice.getInvoiceAmount();
+
+        invoice.setQty( -2 );
+
+        invoice.setItemPrice( -1 );
+
+        System.out.println( invoice );
+
+        System.out.println( getAmount );
 
     }
 
