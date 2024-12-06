d8bbd26c449479b471a9ea4e4f1f37a7d97c8425
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-06 10:53:30 -0300

    lemme cook

---
+++ POO/somexercises/app/MainEmployeeUpgrade.java
@@ -0,0 +1,18 @@
+package POO.somexercises.app;
+
+
+public class MainEmployeeUpgrade {
+
+    public static void main ( String[] args ) {
+
+        mainOperator();
+
+    }
+
+    private static void mainOperator () {
+
+
+
+    }
+
+}
