63336ca7764419cd79308fc23ab81829b07a8569
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-07 11:42:49 -0300

    hmmm

--- POO/moreexercises/entities/HeartRatesBirth.java
+++ POO/moreexercises/entities/HeartRatesBirth.java
@@ -1,4 +1,50 @@
 package POO.moreexercises.entities;
 
+import java.time.LocalDate;
+
 public class HeartRatesBirth {
+
+    private int yearBirth;
+
+    private int monthBirth;
+
+    private int dayBirth;
+
+    public HeartRatesBirth ( int yearBirth, int monthBirth, int dayBirth ) {
+
+        this.dayBirth = dayBirth;
+
+        this.monthBirth = monthBirth;
+
+        this.yearBirth = yearBirth;
+
+    }
+
+    public void getUserAge () {
+
+        
+
+    }
+
+    public int fcMaxCalculus () {
+
+        return 220 - yearBirth;
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append(dayBirth).append("/")
+                .append(monthBirth).append("/")
+                .append(yearBirth);
+
+        return sb.toString();
+
+    }
+
 }
