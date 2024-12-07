40f5ef2866c33e26784cfb50ebb1ddec0872c771
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-07 11:33:49 -0300

    HeartRates class done

--- POO/moreexercises/app/Program.java
+++ POO/moreexercises/app/Program.java
@@ -1,4 +1,15 @@
 package POO.moreexercises.app;
 
+import POO.moreexercises.entities.HeartRatesBirth;
+
+import POO.moreexercises.entities.HeartRates;
+
 public class Program {
+
+    public static void main ( String[] args ) {
+
+
+
+    }
+
 }
--- POO/moreexercises/entities/HeartRates.java
+++ POO/moreexercises/entities/HeartRates.java
@@ -1,4 +1,64 @@
 package POO.moreexercises.entities;
 
 public class HeartRates {
+
+    private String name;
+
+    private String lastName;
+
+    private HeartRatesBirth ratesBirth;
+
+    public HeartRates ( String name, String lastName, HeartRatesBirth ratesBirth ) {
+
+        this.name = name;
+
+        this.lastName = lastName;
+
+        this.ratesBirth = ratesBirth;
+
+    }
+
+    public void setName ( String name ) {
+
+        if ( name.isEmpty() ) {
+
+            this.name = "Default";
+
+            return;
+
+        }
+
+        this.name = name;
+
+    }
+
+    public void setLastName ( String lastName ) {
+
+        if ( lastName.isEmpty() ) {
+
+            this.lastName = "Default";
+
+            return;
+
+        }
+
+        this.lastName = lastName;
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append("Name: ").append(name)
+                .append(" Last Name: ").append(lastName)
+                .append(ratesBirth);
+
+        return sb.toString();
+
+    }
+
 }
