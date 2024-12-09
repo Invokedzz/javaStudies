02b84aff24d8dde65ad468da783b97e444da8cf1
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-09 10:29:14 -0300

    morning

---
+++ POO/control/app/Testing.java
@@ -0,0 +1,45 @@
+package POO.control.app;
+
+import java.util.Scanner;
+
+public class Testing {
+
+    public static void main ( String[] args ) {
+
+        Scanner init = new Scanner ( System.in );
+
+        int getResult = mainApp(init);
+
+        System.out.println(getResult);
+
+    }
+
+    private static int mainApp (Scanner init) {
+
+        int addStudentsGrades, numberOfStudents;
+
+        int total = 0;
+
+        int totalStudentsGrade = 0;
+
+        numberOfStudents = init.nextInt();
+
+        while ( numberOfStudents <= 9 ) {
+
+            System.out.println ( "Enter the grade for this student: ");
+
+            addStudentsGrades = init.nextInt();
+
+            totalStudentsGrade  += addStudentsGrades;
+
+            total = totalStudentsGrade / numberOfStudents;
+
+            numberOfStudents++;
+
+        }
+
+        return total;
+
+    }
+
+}
