11ce67c685261d722511bb3cf5b7ece97a3fe51b
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-10 13:45:08 -0300

    learning about heritage

---
+++ heritage/app/Program.java
@@ -0,0 +1,21 @@
+package heritage.app;
+
+import heritage.entities.BusinessAccount;
+
+import heritage.entities.Account;
+
+public class Program {
+
+    public static void main ( String[] args ) {
+
+        mainApplication();
+
+    }
+
+    private static void mainApplication () {
+
+
+
+    }
+
+}
---
+++ heritage/entities/BusinessAccount.java
@@ -0,0 +1,4 @@
+package heritage.entities;
+
+public class BusinessAccount extends Account {
+}
