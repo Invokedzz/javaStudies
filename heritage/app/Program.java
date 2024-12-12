e47334b0ad462ed4d23623e03af88e35149994f8
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-12 11:35:10 -0300

    oke

--- heritage/app/Program.java
+++ heritage/app/Program.java
@@ -14,9 +14,20 @@ public class Program {
 
     }
 
-    private static void mainApplication () {
+    private static void mainApplication () throws ClassCastException {
 
+        Account account = new Account( "Mr.Duck", 1000.0, 100 );
 
+        Account account1 = new BusinessAccount( "Mr.Goose", 1200.0, 99, 500 );
+
+    //    BusinessAccount account2 = ( BusinessAccount ) new Account( "Mr.Frog", 1500.0, 67 );
+
+        Account account2 = new SavingsAccount();
+
+        System.out.printf( "%s\n%s", account, account1 );
+
+        // Account -> BusinessAccount and SavingsAccount
+        // but BusinessAccount and SavingsAccount can't Account 
 
     }
 
--- heritage/entities/BusinessAccount.java
+++ heritage/entities/BusinessAccount.java
@@ -4,13 +4,13 @@ public class BusinessAccount extends Account {
 
     // holder ( String ), balance ( double ), number ( int )
 
-    private final double loanLimit;
+    private double loanLimit;
 
-    /* public BusinessAccount () {
+     public BusinessAccount () {
 
         super();
 
-    } */
+    }
 
     public BusinessAccount ( String holder, double balance, int number, double loanLimit ) {
 
@@ -31,6 +31,19 @@ public class BusinessAccount extends Account {
         }
 
         System.out.println("invalid amount!");
+
+    }
+
+    @Override
+    public String toString () {
+
+         StringBuilder sb;
+
+         sb = new StringBuilder();
+
+         sb.append(balance);
+
+         return sb.toString();
 
     }
 
