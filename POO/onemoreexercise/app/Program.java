9c05bacaa691dbf598ffbce4244d898d75aded84
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-13 17:27:21 -0300

    done

--- POO/onemoreexercise/app/Program.java
+++ POO/onemoreexercise/app/Program.java
@@ -6,7 +6,13 @@ import POO.onemoreexercise.entities.UsedProduct;
 
 import POO.onemoreexercise.entities.Product;
 
+import java.time.format.DateTimeFormatter;
+
 import java.util.Scanner;
+
+import java.util.List;
+
+import java.util.ArrayList;
 
 import java.time.LocalDate;
 
@@ -21,6 +27,56 @@ public class Program {
     }
 
     private static void mainOperation ( Scanner init ) {
+
+        List < Product > productList = new ArrayList<>();
+
+        System.out.println( "Enter the number of products: " );
+
+        int numberOfProducts = init.nextInt();
+
+        for ( int i = 0; i < numberOfProducts; i++ ) {
+
+            System.out.printf("Product #%s data: ", i + 1 );
+
+            System.out.println( "Common, used or imported (c/u/i)?" );
+
+            char decision = init.next().charAt(0);
+
+            System.out.println( "Name: " );
+
+            String name = init.next();
+
+            System.out.println( "Price: " );
+
+            double price = init.nextDouble();
+
+            if ( decision == 'c') productList.add( new Product( name, price ) );
+
+            if ( decision == 'i' ) {
+
+                double customFee = init.nextDouble();
+
+                productList.add( new ImportedProduct( name, price, customFee ) );
+
+            }
+
+            if ( decision == 'u' ) {
+
+                LocalDate manufactureDate = LocalDate.parse(init.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                productList.add( new UsedProduct( name, price, manufactureDate ) );
+
+            }
+
+        }
+
+        System.out.println("Products: ");
+
+        for ( Product product : productList ) {
+
+            System.out.println(product.priceTag());
+
+        }
 
     }
 
