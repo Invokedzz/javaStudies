49a6c6f2ec7b9ef5b9c840408e0f362e06fb95b6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-13 11:48:01 -0300

    importing a bunch of stuff

---
+++ POO/onemoreexercise/app/Program.java
@@ -0,0 +1,27 @@
+package POO.onemoreexercise.app;
+
+import POO.onemoreexercise.entities.ImportedProduct;
+
+import POO.onemoreexercise.entities.UsedProduct;
+
+import POO.onemoreexercise.entities.Product;
+
+import java.util.Scanner;
+
+import java.time.LocalDate;
+
+public class Program {
+
+    public static void main ( String[] args ) {
+
+        Scanner init = new Scanner ( System.in );
+
+        mainOperation ( init );
+
+    }
+
+    private static void mainOperation ( Scanner init ) {
+        
+    }
+
+}
