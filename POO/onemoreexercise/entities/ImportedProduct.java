f92c135ea9e90c4ba8b7d76ae8d81d5e50bfc1cd
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-13 11:31:43 -0300

    creating constructors

--- POO/onemoreexercise/entities/ImportedProduct.java
+++ POO/onemoreexercise/entities/ImportedProduct.java
@@ -1,4 +1,21 @@
 package POO.onemoreexercise.entities;
 
 public class ImportedProduct extends Product {
+
+    private double customsFee;
+
+    public ImportedProduct () {
+
+        super ();
+
+    }
+
+    public ImportedProduct ( String name, double price, double customsFee ) {
+
+        super( name, price );
+
+        this.customsFee = customsFee;
+
+    }
+
 }
--- POO/onemoreexercise/entities/UsedProduct.java
+++ POO/onemoreexercise/entities/UsedProduct.java
@@ -1,4 +1,23 @@
 package POO.onemoreexercise.entities;
 
+import java.time.LocalDate;
+
 public class UsedProduct extends Product {
+
+    private LocalDate manufactureDate;
+
+    public UsedProduct () {
+
+        super ();
+
+    }
+
+    public UsedProduct ( String name, double price, LocalDate manufactureDate ) {
+
+        super ( name, price );
+
+        this.manufactureDate = manufactureDate;
+
+    }
+
 }
