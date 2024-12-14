12a12495e34736baf645614571b12a2923a00c60
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-14 19:48:53 -0300

    hmmmm

--- heritage/randomstuff/app/Program.java
+++ heritage/randomstuff/app/Program.java
@@ -1,5 +1,7 @@
 package heritage.randomstuff.app;
 
+import heritage.randomstuff.entities.Account;
+
 import heritage.randomstuff.entities.BusinessAccount;
 
 import heritage.randomstuff.entities.SavingsAccount;
@@ -14,7 +16,62 @@ public class Program {
 
     public static void main ( String[] args ) {
 
+        Scanner init = new Scanner ( System.in );
 
+        mainOperation ( init );
+
+    }
+
+    private static void mainOperation ( Scanner init ) {
+
+        int numberOfAccounts = init.nextInt();
+
+        for ( int i = 0; i < numberOfAccounts; i ++ ) {
+
+            // holder (String), number (int), balance (double)
+
+            System.out.println( "Enter the holder name: " );
+
+            String holder = init.next();
+
+            System.out.println( "Enter your account number: " );
+
+            int number = init.nextInt();
+
+            System.out.println( "Enter the initial balance: " );
+
+            double balance = init.nextDouble();
+
+            System.out.println( "Saving or Business Account? (b/s): " );
+
+            char userChoice = init.next().charAt( 0 );
+
+            optionsSelectedByUser( holder, number, balance, userChoice, init );
+
+        }
+
+    }
+
+    private static void optionsSelectedByUser ( String holder, int number, double balance,
+                                                char userChoice, Scanner init ) {
+
+        List < Account > accountList = new ArrayList<>();
+
+        if ( userChoice == 'b' ) {
+
+            double loanLimit = init.nextDouble();
+
+            accountList.add( new BusinessAccount( holder, number, balance, loanLimit ) );
+
+        }
+
+        if ( userChoice == 's' ) {
+
+            double interestRate = init.nextDouble();
+
+            accountList.add( new SavingsAccount( holder, number, balance, interestRate ) );
+
+        }
 
     }
 
