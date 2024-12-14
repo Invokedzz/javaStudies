18f8658ff9c9ec08ee0fffa6c1b74369ec7d7a9b
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-14 11:36:51 -0300

    BusinessAccount class

---
+++ heritage/randomstuff/app/Program.java
@@ -0,0 +1,21 @@
+package heritage.randomstuff.app;
+
+import heritage.randomstuff.entities.BusinessAccount;
+
+import heritage.randomstuff.entities.SavingsAccount;
+
+import java.util.Scanner;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class Program {
+
+    public static void main ( String[] args ) {
+
+
+
+    }
+
+}
--- heritage/randomstuff/entities/BusinessAccount.java
+++ heritage/randomstuff/entities/BusinessAccount.java
@@ -1,4 +1,50 @@
 package heritage.randomstuff.entities;
 
 public class BusinessAccount extends Account {
+
+    private double loanLimit;
+
+    public BusinessAccount () {
+
+        super ();
+
+    }
+
+    public BusinessAccount ( String holder, Integer number, double balance, double loanLimit ) {
+
+        super ( holder, number, balance );
+
+        this.loanLimit = loanLimit;
+
+    }
+
+    public double loan ( double amount ) {
+
+        if ( loanLimit <= amount ) {
+
+            double balance = getBalance();
+
+            return balance - amount;
+            
+        }
+
+        return getBalance();
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( "Holder: " ).append( getHolder() )
+                .append( " Number: " ).append( getNumber() )
+                .append( " Balance: " ).append( getBalance() );
+
+        return sb.toString();
+
+    }
+
 }
