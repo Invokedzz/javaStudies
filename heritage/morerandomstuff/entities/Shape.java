8d0f8287d5a0f0d02ce400e90f8aa10f1a67a78c
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-16 10:37:53 -0300

    class Rectangle

--- heritage/morerandomstuff/entities/Shape.java
+++ heritage/morerandomstuff/entities/Shape.java
@@ -1,7 +1,25 @@
 package heritage.morerandomstuff.entities;
 
+import heritage.morerandomstuff.entities.enums.Color;
+
 public abstract class Shape {
 
+    private Color color;
+
     public Shape () {}
+
+    public Shape ( Color color ) {
+
+        this.color = color;
+
+    }
+
+    public Color getColor () {
+
+        return color;
+
+    }
+
+    public abstract double area ();
 
 }
