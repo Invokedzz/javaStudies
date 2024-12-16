28cb352eeadf0fdf6340b26f79aac0a4314964ff
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-16 14:56:04 -0300

    handling stuff lul

---
+++ exceptions/firstclass/IDontKnow.java
@@ -0,0 +1,40 @@
+package exceptions.firstclass;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class IDontKnow {
+
+    public static void main ( String[] args ) {
+
+        Scanner init = new Scanner ( System.in );
+
+        mainOperation ( init );
+
+    }
+
+    private static void mainOperation ( Scanner init ) {
+
+        // we need to use "nextLine()" in order to insert a bunch of names
+        // ArrayIndexOutOfBoundsException
+        // InputMismatchException
+
+        try {
+
+            String [] vector = init.nextLine().split(" ");
+
+            int certainElement = init.nextInt();
+
+            System.out.println(vector[certainElement]);
+
+        } catch (InputMismatchException e) {
+
+            String [] handleError = new String[] { "James", "Paul", "Peter" };
+
+            System.out.println( handleError [ 1 ] );
+
+        }
+
+    }
+
+}
