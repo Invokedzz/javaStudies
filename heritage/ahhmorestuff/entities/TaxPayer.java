68e6b1056d2cfac207c7f0b6e603a5963580e8bc
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-16 13:46:16 -0300

    done

--- heritage/ahhmorestuff/entities/TaxPayer.java
+++ heritage/ahhmorestuff/entities/TaxPayer.java
@@ -24,4 +24,18 @@ public abstract class TaxPayer {
 
     public abstract Double tax ();
 
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( "Name: " ).append( name )
+                .append( " Income: " ).append( income );
+
+        return sb.toString();
+
+    }
+
 }
