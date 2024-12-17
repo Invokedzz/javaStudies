c078affbcd6b5009b0d08f145594aaf9416801a6
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-17 13:27:07 -0300

    working on Reservation class

--- exceptions/fifthclass/model/entities/Reservation.java
+++ exceptions/fifthclass/model/entities/Reservation.java
@@ -9,4 +9,56 @@ import java.time.LocalDate;
 import exceptions.fifthclass.model.exceptions.DomainException;
 
 public class Reservation {
+
+    private Integer roomNumber;
+
+    private LocalDate checkIn;
+
+    private LocalDate checkOut;
+
+    public Reservation () {}
+
+    public Reservation ( Integer roomNumber, LocalDate checkIn, LocalDate checkOut ) {
+
+        this.roomNumber = roomNumber;
+
+        this.checkIn = checkIn;
+
+        this.checkOut = checkOut;
+
+    }
+
+    public Integer getRoomNumber () {
+
+        return roomNumber;
+
+    }
+
+    public LocalDate getCheckIn () {
+
+        return checkIn;
+
+    }
+
+    public LocalDate getCheckOut () {
+
+        return checkOut;
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( "Reservation: Room " ).append( getRoomNumber() )
+                .append( "," ).append( " check-in: " ).append( getCheckIn() )
+                .append( "," ).append( " check-out: " ).append( getCheckOut() );
+
+        return sb.toString();
+
+    }
+
 }
