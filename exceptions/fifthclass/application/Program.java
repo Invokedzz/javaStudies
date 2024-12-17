ffeb58e2848b9b371dee1cec2b0a111939f6eb7c
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-17 16:09:51 -0300

    exceptions

--- exceptions/fifthclass/application/Program.java
+++ exceptions/fifthclass/application/Program.java
@@ -1,10 +1,15 @@
 package exceptions.fifthclass.application;
 
 import exceptions.fifthclass.model.entities.Reservation;
+import exceptions.fifthclass.model.exceptions.InvalidDatePeriod;
 
 import java.text.ParseException;
 
 import java.text.SimpleDateFormat;
+
+import java.util.List;
+
+import java.util.ArrayList;
 
 import java.util.Scanner;
 
@@ -20,17 +25,69 @@ public class Program {
 
     }
 
-    private static void mainOperations ( Scanner init ) throws ParseException {
+    private static void mainOperations ( Scanner init ) {
+
+        List < Reservation > reservationList = new ArrayList<>();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat( "dd/MM/yyyy" );
 
-        Date date = simpleDateFormat.parse( init.next());
+        System.out.println( "Enter the number of rooms you want to rent: " );
 
-        Date dateNumberTu = simpleDateFormat.parse( init.next() );
+        int numberOfRooms = init.nextInt();
 
-        Reservation reservation = new Reservation( 903, date, dateNumberTu );
+        try {
 
-        System.out.println( reservation );
+            for ( int index = 0; index < numberOfRooms; index ++ ) {
+
+                System.out.println( "Enter the room number: " );
+
+                Integer roomNumber = init.nextInt();
+
+                System.out.println( "Enter the check-in: " );
+
+                Date checkIn = simpleDateFormat.parse( init.next());
+
+                System.out.println( "Enter the check-out: " );
+
+                Date checkOut = simpleDateFormat.parse( init.next() );
+
+                validateDatePeriod( checkIn, checkOut );
+
+                Reservation reservation = new Reservation( roomNumber, checkIn, checkOut );
+
+                reservationList.add( reservation );
+
+            }
+
+            for ( Reservation reservation : reservationList ) {
+
+                System.out.printf( "Stats: %s ", reservation );
+
+            }
+
+        } catch ( ParseException error ) {
+
+            System.out.println( "The parse was not done correctly. Try again!" );
+
+            return;
+
+        } catch ( InvalidDatePeriod error ) {
+
+            System.out.printf( "Error: %s", error.getMessage() );
+
+        }
+
+        System.out.println( "The registration was done successfully!" );
+
+    }
+
+    // if (date.after( dateNumberTu )) System.out.println("Hii"); // Handle this error
+
+    private static void validateDatePeriod ( Date checkIn, Date checkOut ) throws InvalidDatePeriod {
+
+        if ( checkIn.after( checkOut ) ) throw new InvalidDatePeriod( "Enter a valid date!" );
+
+        System.out.println();
 
     }
 
---
+++ exceptions/fifthclass/model/exceptions/InvalidDatePeriod.java
@@ -0,0 +1,9 @@
+package exceptions.fifthclass.model.exceptions;
+
+public class InvalidDatePeriod extends Exception {
+
+    public InvalidDatePeriod ( String message ) {
+        super ( message );
+    }
+
+}
