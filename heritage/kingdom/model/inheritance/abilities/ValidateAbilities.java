86c4436a2815f8207d86abbc422f7b946fe583cf
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-19 15:51:13 -0300

    hmmm

--- heritage/kingdom/model/inheritance/abilities/ValidateAbilities.java
+++ heritage/kingdom/model/inheritance/abilities/ValidateAbilities.java
@@ -12,7 +12,7 @@ import heritage.kingdom.model.inheritance.abilitiesenum.Abilities;
 
 // MAGICAL_BOOST, STRENGTH_BOOST, WRATH_BOOST,
 
-// DEFENSE_BOOST, HEALING_BOOST, COOKING_BOOST, FISHING_BOOST
+// DEFENSE_BOOST, HEALING_BOOST
 
 public class ValidateAbilities {
 
@@ -20,7 +20,7 @@ public class ValidateAbilities {
 
         if ( knight.getSpecialAbility().equals ( Abilities.STRENGTH_BOOST ) ) return knight.getAttackPoints() + 50.0;
 
-        if ( knight.getSpecialAbility().equals ( Abilities.DEFENSE_BOOST ) ) return knight.getDefensePoints() + 30;
+        if ( knight.getSpecialAbility().equals ( Abilities.DEFENSE_BOOST ) ) return knight.getDefensePoints() + 30.0;
 
         return knight.getAttackPoints();
 
@@ -28,14 +28,30 @@ public class ValidateAbilities {
 
     public double abilitiesBarbarian ( Barbarian barbarian ) {
 
+        if ( barbarian.getSpecialAbility().equals ( Abilities.WRATH_BOOST ) ) return barbarian.getFury() + 100.0;
+
+        if ( barbarian.getSpecialAbility().equals ( Abilities.STRENGTH_BOOST ) ) return barbarian.getAttackPoints() + 50.0;
+
+        return barbarian.getAttackPoints();
+
     }
 
     public double abilitiesMage ( Mage mage ) {
 
+        if ( mage.getSpecialAbility().equals ( Abilities.MAGICAL_BOOST ) ) return mage.getAttackPoints() + mage.getMana();
+
+        return mage.getAttackPoints();
+
     }
 
     public double abilitiesPaladin ( Paladin paladin ) {
-        
+
+        if ( paladin.getSpecialAbility().equals ( Abilities.DEFENSE_BOOST ) ) return paladin.getDefensePoints() + 50.0;
+
+        if ( paladin.getSpecialAbility().equals ( Abilities.HEALING_BOOST ) ) return paladin.getAttackPoints() + paladin.getDivineStrike();
+
+        return paladin.getAttackPoints();
+
     }
 
 }
