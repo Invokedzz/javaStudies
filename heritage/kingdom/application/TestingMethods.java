f09d9e08058c01944bd90983bcf624ac9e5c5a65
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-19 09:46:56 -0300

    testing attack/defense methods

--- heritage/kingdom/application/TestingMethods.java
+++ heritage/kingdom/application/TestingMethods.java
@@ -8,19 +8,49 @@ public class TestingMethods {
 
     public static void main ( String[] args ) {
 
-        testLevelOfCharacter();
+        Scanner init = new Scanner ( System.in );
+
+        testLevelOfCharacter ( init );
+
+        System.out.println( "##############################" );
+
+        testAttackOfCharacter ( init );
+
+        System.out.println( "##############################" );
+
+        testDefenseOfCharacter ( init );
+
+        System.out.println( "##############################" );
 
     }
 
-    private static void testLevelOfCharacter () {
-
-        Scanner init = new Scanner( System.in );
+    private static void testLevelOfCharacter ( Scanner init ) {
 
         AbilitiesCharacter abilitiesCharacter = new AbilitiesCharacter();
 
-        int getLevel = abilitiesCharacter.levelValidation( init );
+        int getLevel = abilitiesCharacter.levelValidation ( init );
 
-        System.out.println( getLevel );
+        System.out.printf ( "Level: %s", getLevel );
+
+    }
+
+    private static void testAttackOfCharacter ( Scanner init ) {
+
+        AbilitiesCharacter abilitiesCharacter = new AbilitiesCharacter();
+
+        double getAttackPoints = abilitiesCharacter.attackValidation ( init );
+
+        System.out.printf ( "Attack Points: %s", getAttackPoints );
+
+    }
+
+    private static void testDefenseOfCharacter ( Scanner init ) {
+
+        AbilitiesCharacter character = new AbilitiesCharacter();
+
+        double getDefensePoints = character.defensePoints ( init );
+
+        System.out.printf ( "Defense Points: %s", getDefensePoints );
 
     }
 
