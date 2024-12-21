02289141eb608ea8b616926f1195a2f3004d804f
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-21 11:08:06 -0300

    looking good

--- exceptions/seventhclass/application/Application.java
+++ exceptions/seventhclass/application/Application.java
@@ -1,6 +1,8 @@
 package exceptions.seventhclass.application;
 
 import exceptions.seventhclass.model.entities.Account;
+import exceptions.seventhclass.model.exceptions.InvalidBalanceException;
+import exceptions.seventhclass.model.exceptions.InvalidLimitException;
 
 import java.util.Scanner;
 
@@ -19,7 +21,41 @@ public class Application {
     }
 
     private static void mainOperations ( Scanner init ) {
-        
+
+        try {
+
+            System.out.println( "Enter account data" );
+
+            System.out.println( "Number: " );
+
+            Integer number = init.nextInt();
+
+            System.out.println( "Holder: " );
+
+            String holder = init.next();
+
+            System.out.println( "Initial Balance: " );
+
+            double balance = init.nextDouble();
+
+            System.out.println( "Withdraw Limit: \n" );
+
+            Double withdrawLimit = init.nextDouble();
+
+            Account account = new Account( number, holder, balance, withdrawLimit );
+
+            System.out.println("Enter amount for withdraw: ");
+
+            double amount = init.nextDouble();
+
+            System.out.println( account.withdraw( amount ) );
+
+        } catch ( InvalidBalanceException | InvalidLimitException error ) {
+
+            System.out.println( error.getMessage() );
+
+        }
+
     }
 
 }
