750fdc123b09bc825c992733277adfe2166973ee
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-24 12:16:43 -0300

    entities done

---
+++ filejava/fifth/app/Program.java
@@ -0,0 +1,29 @@
+package filejava.fifth.app;
+
+import filejava.fifth.model.entities.Worker;
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+
+import java.io.BufferedWriter;
+
+import java.io.FileWriter;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main ( String[] args ) {
+
+        Scanner init = new Scanner ( System.in );
+
+        programStart ( init );
+
+    }
+
+    private static void programStart ( Scanner init ) {
+
+    }
+
+}
---
+++ filejava/fifth/model/entities/Worker.java
@@ -0,0 +1,56 @@
+package filejava.fifth.model.entities;
+
+public class Worker {
+
+    private String worker;
+
+    private Integer id;
+
+    private String department;
+
+    public Worker () {}
+
+    public Worker ( String worker, Integer id, String department ) {
+
+        this.worker = worker;
+
+        this.id = id;
+
+        this.department = department;
+
+    }
+
+    public String getWorker () {
+
+        return worker;
+
+    }
+
+    public Integer getId () {
+
+        return id;
+
+    }
+
+    public String getDepartment () {
+
+        return department;
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( "Name: " ).append( getWorker() )
+                .append( " id: " ).append( getId() )
+                .append( " Department: " ).append( getDepartment() );
+
+        return sb.toString();
+
+    }
+
+}
