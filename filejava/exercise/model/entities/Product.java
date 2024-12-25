e73efcfa3fb6e79b1efdd21d451fcb6dbddaa3ff
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-25 16:30:51 -0300

    creating entities - Product

--- filejava/exercise/model/entities/Product.java
+++ filejava/exercise/model/entities/Product.java
@@ -1,4 +1,61 @@
 package filejava.exercise.model.entities;
 
 public class Product {
+
+    private String productName;
+
+    private double productPrice;
+
+    private int productQuantity;
+
+    public Product () {}
+
+    public Product ( String productName, double productPrice, int productQuantity ) {
+
+        this.productName = productName;
+
+        this.productPrice = productPrice;
+
+        this.productQuantity = productQuantity;
+
+    }
+
+    public String getProductName () {
+
+        return productName;
+
+    }
+
+    public double getProductPrice () {
+
+        return productPrice;
+
+    }
+
+    public int getProductQuantity () {
+
+        return productQuantity;
+
+    }
+
+    public double pricePerQuantity () {
+
+        return getProductPrice() * getProductQuantity();
+
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( "Name: " ).append( getProductName() )
+                .append( " Price: " ).append( getProductPrice() );
+
+        return sb.toString();
+
+    }
+
 }
