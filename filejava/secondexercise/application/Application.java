388d5d3d9fdd0cf3915f3de502f67e460822b44a
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-26 11:42:29 -0300

    end

--- filejava/secondexercise/application/Application.java
+++ filejava/secondexercise/application/Application.java
@@ -22,6 +22,8 @@ public class Application {
 
     private static void letsRunThisProgram ( Scanner sc ) {
 
+        List < Product > productList = new ArrayList<>();
+
         File filePath = new File( "/Users/samunoinv/Documents/GitHub/javaStudies/filejava/secondexercise/model/input.csv" );
 
         String getFolderPath = filePath.getParent();
@@ -30,23 +32,41 @@ public class Application {
 
         boolean wasFolderCreated = prepareANewPath.mkdir();
 
-        System.out.println( "Created: " + wasFolderCreated );
+        getMessageForFileCreation( wasFolderCreated );
 
-        try ( BufferedWriter bufferedWriter = new BufferedWriter( new FileWriter ( prepareANewPath + "/cool" ) ) ) {
+        try ( BufferedWriter bufferedWriter = new BufferedWriter( new FileWriter ( prepareANewPath + "/cool", true ) ) ) {
+
+            System.out.println( "Enter the number of products: " );
 
             int numberOfProducts = sc.nextInt();
 
             for ( int index = 0; index < numberOfProducts; index ++ ) {
 
+                System.out.println( "Enter the product name: " );
+
                 String productName = sc.next();
 
+                System.out.println( "Enter the product price: " );
+
                 Double productPrice = sc.nextDouble();
+
+                System.out.println( "Enter the product quantity: " );
 
                 Integer productQuantity = sc.nextInt();
 
                 Product product = new Product( productName, productPrice, productQuantity );
 
                 bufferedWriter.write( product.toString() );
+
+                productList.add( product );
+
+            }
+
+            bufferedWriter.close();
+
+            for ( Product products : productList ) {
+
+                System.out.printf( "Product: %s", products );
 
             }
 
@@ -55,6 +75,20 @@ public class Application {
             System.out.printf( "Error: %s", error.getMessage() );
 
         }
+
+    }
+
+    private static void getMessageForFileCreation ( boolean wasFolderCreated ) {
+
+        if ( !wasFolderCreated ) {
+
+            System.out.println( "Was created!" );
+
+            return;
+
+        }
+
+        System.out.printf( "Created: %s", true );
 
     }
 
