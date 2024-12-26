b13f845d4e79c82d21ef0d4fb96432111fe1a806
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-26 14:48:51 -0300

    CarRental class

--- interfaces/nointerfaceexercise/model/entities/CarRental.java
+++ interfaces/nointerfaceexercise/model/entities/CarRental.java
@@ -1,4 +1,77 @@
 package interfaces.nointerfaceexercise.model.entities;
 
+import java.time.LocalDateTime;
+
 public class CarRental {
+
+    private LocalDateTime start; // 2007-12-03T10:15:30
+
+    private LocalDateTime finish;
+
+    private Invoice invoice;
+
+    private Vehicle vehicle;
+
+    public CarRental () {}
+
+    public CarRental ( LocalDateTime start, LocalDateTime finish, Invoice invoice, Vehicle vehicle ) {
+
+        this.start = start;
+
+        this.finish = finish;
+
+        this.invoice = invoice;
+
+        this.vehicle = vehicle;
+
+    }
+
+    public LocalDateTime getStart () {
+
+        return start;
+
+    }
+
+    public void setStart ( LocalDateTime start ) {
+
+        this.start = start;
+
+    }
+
+    public LocalDateTime getFinish () {
+
+        return finish;
+
+    }
+
+    public void setFinish ( LocalDateTime finish ) {
+
+        this.finish = finish;
+
+    }
+
+    public Invoice getInvoice () {
+
+        return invoice;
+
+    }
+
+    public void setInvoice ( Invoice invoice ) {
+
+        this.invoice = invoice;
+
+    }
+
+    public Vehicle getVehicle () {
+
+        return vehicle;
+
+    }
+
+    public void setVehicle ( Vehicle vehicle ) {
+
+        this.vehicle = vehicle;
+
+    }
+
 }
