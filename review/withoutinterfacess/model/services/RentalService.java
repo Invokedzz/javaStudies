229056f2dc3b1953a169de1f528efe2b0de1be88
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-27 15:04:34 -0300

    services

--- review/withoutinterfacess/model/services/RentalService.java
+++ review/withoutinterfacess/model/services/RentalService.java
@@ -1,4 +1,39 @@
 package review.withoutinterfacess.model.services;
 
+import review.withoutinterfacess.model.entities.Invoice;
+
+import review.withoutinterfacess.model.entities.MotorcycleRent;
+
+import java.time.Duration;
+
 public class RentalService {
+
+    private Double valuePerHour;
+
+    private USTax usTax;
+
+    public RentalService () {}
+
+    public RentalService ( Double valuePerHour, USTax usTax ) {
+
+        this.valuePerHour = valuePerHour;
+
+        this.usTax = usTax;
+
+    }
+
+    public void processInvoice ( MotorcycleRent carRental ) {
+
+        double minutes = Duration.between( carRental.getStart(), carRental.getFinish() ).toMinutes();
+
+        double hours = valuePerHour * minutes / 60.0;
+
+        double payment = carRental.getInvoice().getPayment() + hours;
+
+        double tax = usTax.determineTax( payment );
+
+        carRental.setInvoice( new Invoice ( payment, tax ) );
+
+    }
+
 }
--- review/withoutinterfacess/model/services/USTax.java
+++ review/withoutinterfacess/model/services/USTax.java
@@ -1,4 +1,15 @@
 package review.withoutinterfacess.model.services;
 
 public class USTax {
+
+    public double determineTax ( Double amount ) {
+
+        double dollarConverter = amount * 6.20;
+
+        if ( dollarConverter < 50.0 ) return dollarConverter * 0.20;
+
+        return dollarConverter * 0.60;
+
+    }
+
 }
