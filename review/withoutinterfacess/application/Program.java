039c829718a2f34f4c5d51ba578286b8883359df
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-27 15:30:38 -0300

    adding some inputs

--- review/withoutinterfacess/application/Program.java
+++ review/withoutinterfacess/application/Program.java
@@ -22,6 +22,40 @@ public class Program {
 
     public static void main ( String[] args ) {
 
+        Scanner sc = new Scanner( System.in );
+
+        runProgram ( sc );
+
+    }
+
+    private static void runProgram ( Scanner sc ) {
+
+        List < MotorcycleRent > motorcycleRents = new ArrayList<>();
+
+        LocalDateTime start = startOfTheRent( sc );
+
+        LocalDateTime finish = endOfTheRent( sc );
+
+        Vehicle vehicle = new Vehicle( VehicleModel.KAWASAKI, VehicleColor.BLACK, 123 );
+
+        MotorcycleRent motorcycleRent = new MotorcycleRent( start, finish, new Invoice(), vehicle );
+
+    }
+
+    private static LocalDateTime startOfTheRent ( Scanner sc ) {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern( "dd/MM/yyy HH:mm" );
+
+        return LocalDateTime.parse( sc.nextLine(), dateTimeFormatter );
+
+    }
+
+    private static LocalDateTime endOfTheRent ( Scanner sc ) {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern( "dd/MM/yyy HH:mm" );
+
+        return LocalDateTime.parse( sc.nextLine(), dateTimeFormatter );
+
     }
 
 }
