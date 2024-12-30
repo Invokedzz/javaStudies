3845160958467aafc154a72cfe7282323d686882
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-30 12:33:47 -0300

    finish entities

--- review/withoutinterfacessagain/model/entities/Order.java
+++ review/withoutinterfacessagain/model/entities/Order.java
@@ -1,4 +1,71 @@
 package review.withoutinterfacessagain.model.entities;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+import java.time.LocalDateTime;
+
 public class Order {
+
+    private LocalDateTime moment;
+
+    private OrderStatus status;
+
+    private OrderItem orderItem;
+
+    private Client client;
+
+    List < Order > orderList = new ArrayList<>();
+
+    public Order () {}
+
+    public Order ( LocalDateTime moment, OrderStatus status, OrderItem orderItem, Client client ) {
+
+        this.moment = moment;
+
+        this.status = status;
+
+        this.orderItem = orderItem;
+
+        this.client = client;
+
+    }
+
+    public LocalDateTime getMoment () {
+
+        return moment;
+
+    }
+
+    public OrderStatus getStatus () {
+
+        return status;
+
+    }
+
+    public OrderItem getOrderItem () {
+
+        return orderItem;
+
+    }
+
+    public Client getClient () {
+
+        return client;
+
+    }
+
+    public void addItem ( Order order ) {
+
+        orderList.add( order );
+
+    }
+
+    public void removeItem ( Order order ) {
+
+        orderList.remove( order );
+
+    }
+
 }
