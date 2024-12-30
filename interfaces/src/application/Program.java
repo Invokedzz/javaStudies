82e6c8fde660bf2bba6e349ef05c805546a241a0
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-30 15:07:05 -0300

    almost there

--- interfaces/src/application/Program.java
+++ interfaces/src/application/Program.java
@@ -4,11 +4,73 @@ import interfaces.src.model.entities.*;
 
 import interfaces.src.model.services.*;
 
-import interfaces.src.model.exceptions.*;
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main ( String[] args ) {
+
+        Scanner sc = new Scanner( System.in );
+
+        runTheProgramAsshole ( sc );
+
+    }
+
+    private static void runTheProgramAsshole ( Scanner sc ) {
+
+        // 2007-12-03.
+
+        LocalDate date = enterDate( sc );
+
+        int number = enterNumber( sc );
+
+        double totalValue = enterTotalValue( sc );
+
+        int months = enterNumberOfMonths( sc );
+
+        Contract contract = new Contract( date, number, totalValue );
+
+        ContractService contractService = new ContractService( new PaypalService() );
+
+        contractService.processContract( contract, months );
+
+    }
+
+    private static LocalDate enterDate ( Scanner sc ) {
+
+        System.out.println( "Enter a date: " );
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern( "dd/MM/yyyy" );
+
+        return LocalDate.parse( sc.next(), dateTimeFormatter );
+
+    }
+
+    private static int enterNumber ( Scanner sc ) {
+
+        System.out.println( "Enter a number: " );
+
+        return sc.nextInt();
+
+    }
+
+    private static double enterTotalValue ( Scanner sc ) {
+
+        System.out.println( "Enter the total value: " );
+
+        return sc.nextDouble();
+
+    }
+
+    private static int enterNumberOfMonths ( Scanner sc ) {
+
+        System.out.println( "Enter number of months: " );
+
+        return sc.nextInt();
 
     }
 
--- review/withoutinterfacessagain/application/Program.java
+++ review/withoutinterfacessagain/application/Program.java
@@ -4,8 +4,6 @@ import review.withoutinterfacessagain.model.entities.*;
 
 import review.withoutinterfacessagain.model.services.*;
 
-import review.withoutinterfacessagain.model.exceptions.*;
-
 import java.time.*;
 
 import java.util.Scanner;
