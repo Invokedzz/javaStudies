6f7624e257b346c52e3329f43830c05229328b17
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-12-30 14:47:42 -0300

    services

--- interfaces/src/model/services/ContractService.java
+++ interfaces/src/model/services/ContractService.java
@@ -1,4 +1,41 @@
 package interfaces.src.model.services;
 
+import interfaces.src.model.entities.Contract;
+
+import interfaces.src.model.entities.Installment;
+
+import java.time.LocalDate;
+
 public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService () {}
+
+    public ContractService ( OnlinePaymentService onlinePaymentService ) {
+
+        this.onlinePaymentService = onlinePaymentService;
+
+    }
+
+    public void processContract ( Contract contract, int months ) {
+
+        double basicQuota = contract.getTotalValue() / months;
+
+        for ( int index = 1; index <= months; index ++ ) {
+
+            LocalDate date = contract.getDate().plusMonths( index );
+
+            double interest = onlinePaymentService.interest( basicQuota, months );
+
+            double fee = onlinePaymentService.paymentFee( basicQuota );
+
+            double quota = fee + interest + basicQuota;
+
+            contract.getInstallmentList().add( new Installment( date, quota ) );
+
+        }
+
+    }
+
 }
--- interfaces/src/model/services/OnlinePaymentService.java
+++ interfaces/src/model/services/OnlinePaymentService.java
@@ -1,4 +1,9 @@
 package interfaces.src.model.services;
 
 public interface OnlinePaymentService {
+
+    double paymentFee ( double amount );
+
+    double interest ( double amount, int months );
+
 }
