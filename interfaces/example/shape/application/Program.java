5f29fc3bada22315663f5728018d8e1590e0a9e2
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-06 17:41:29 -0300

    end

--- interfaces/example/shape/application/Program.java
+++ interfaces/example/shape/application/Program.java
@@ -1,7 +1,33 @@
 package interfaces.example.shape.application;
 
+import interfaces.example.shape.services.*;
+
+import interfaces.example.shape.entities.*;
+
 public class Program {
 
+    public static void main ( String[] args ) {
 
+        runProgram ();
+
+    }
+
+    private static void runProgram () {
+
+        Circle circle = new Circle( 10.0 );
+
+        Square square = new Square( 2.0 );
+
+        SquareArea squareArea = new SquareArea( square );
+
+        CircleArea circleArea = new CircleArea( circle );
+
+        double result = squareArea.calculateArea();
+
+        double result2 = circleArea.calculateArea();
+
+        System.out.printf("Square Area: %s\nCircle Area: %s", result, result2 );
+
+    }
 
 }
