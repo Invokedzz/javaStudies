8073fe8e7002c7e24d38bfe7a6c556235fbc996f
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-12 20:04:28 -0300

    HashSet

--- generics/set/firstencounter/src/Program.java
+++ generics/set/firstencounter/src/Program.java
@@ -1,14 +1,54 @@
 package generics.set.firstencounter.src;
 
+import java.util.Set;
+
+import java.util.HashSet;
+
+import java.util.Scanner;
+
+import generics.set.firstencounter.entities.Product;
+
 public class Program {
 
     public static void main ( String[] args ) {
 
-        runThisShit();
+        Scanner sc = new Scanner( System.in );
+
+        runThisShit( sc );
 
     }
 
-    private static void runThisShit () {
+    private static void runThisShit ( Scanner sc ) {
+
+        /*This class implements the Set interface, backed by a hash table (actually a HashMap instance).
+                It makes no guarantees as to the iteration order of the set; in particular, it does not guarantee that the order will remain constant over time.
+                This class permits the null element.
+                This class offers constant time performance for the basic operations (add, remove, contains and size),
+                 assuming the hash function disperses the elements properly among the buckets.
+                  Iterating over this set requires time proportional to the sum of the HashSet instance's size (the number of elements)
+                   plus the "capacity" of the backing HashMap instance (the number of buckets).
+                   Thus, it's very important not to set the initial capacity too high (or the load factor too low) if iteration performance is important.
+         */
+
+        Set < Product > stringSet = new HashSet<>(); // faster (o1), it doesn't have an ordered list
+
+        int numberOfElements = sc.nextInt();
+
+        for ( int index = 0; index < numberOfElements; index ++ ) {
+
+            String productName = sc.next();
+
+            String productId = sc.next();
+
+            stringSet.add( new Product( productName, productId ));
+
+        }
+
+        for ( Product item : stringSet ) {
+
+            System.out.println( item );
+
+        }
 
     }
 
