f6d7f4f541833eecc45e9cf5a4171d6b95538cff
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-13 14:46:29 -0300

    zzz

---
+++ Challenge/Challenge16th/src/Program.java
@@ -0,0 +1,19 @@
+package Challenge.Challenge16th.src;
+
+public class Program {
+
+    public static void main ( String[] args ) {
+
+        int result = theMaximumAchievableX( 4, 1 );
+
+        System.out.println( result );
+
+    }
+
+    public static int theMaximumAchievableX(int num, int t) {
+
+        return num + ( t * 2 ); // dude what the hell is this problem???
+
+    }
+
+}
