2c1c2c61207955bc2d4b7af80b9a551068855afc
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-14 10:30:16 -0300

    hmmm

--- generics/map/secondencounter/src/app/Program.java
+++ generics/map/secondencounter/src/app/Program.java
@@ -1,4 +1,69 @@
 package generics.map.secondencounter.src.app;
 
+import generics.map.secondencounter.src.model.entities.Candidate;
+
+import java.io.*;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
 public class Program {
+
+    public static void main ( String[] args ) {
+
+        countVotesAndCandidates();
+
+    }
+
+    private static void countVotesAndCandidates () {
+
+        Map < String, Integer > candidateMap = new HashMap();
+
+        File file = new File( "/Users/samunoinv/Documents/GitHub/javaStudies/generics/map/secondencounter/src/model/files/candidatevotes" );
+
+        try ( BufferedReader bufferedReader = new BufferedReader( new FileReader( file ) )) {
+
+            String line = bufferedReader.readLine();
+
+            while ( line != null ) {
+
+                String [] fields = line.split( "," );
+
+                String candidateName = fields[0];
+
+                Integer votes = Integer.parseInt( fields[1] );
+
+                candidateMap.put( candidateName, votes );
+
+                if ( candidateMap.containsKey(candidateName)) {
+
+                    int totalVotes = candidateMap.get( candidateName );
+
+                    int sum = totalVotes + votes;
+
+                    candidateMap.put( candidateName, sum );
+
+                }
+
+                else candidateMap.put( candidateName, votes );
+
+                line = bufferedReader.readLine();
+
+            }
+
+            for ( String key : candidateMap.keySet() ) {
+
+                System.out.printf( "%s  %s", candidateMap.get( key ), key );
+
+            }
+
+        } catch ( IOException error ) {
+
+            System.out.println( error.getMessage() );
+
+        }
+
+    }
+
 }
--- generics/map/secondencounter/src/model/entities/Candidate.java
+++ generics/map/secondencounter/src/model/entities/Candidate.java
@@ -1,4 +1,58 @@
 package generics.map.secondencounter.src.model.entities;
 
+import java.util.Objects;
+
 public class Candidate {
+
+    private String candidateName;
+
+    private Integer id;
+
+    public Candidate () {}
+
+    public Candidate ( String candidateName, Integer id ) {
+
+        this.candidateName = candidateName;
+
+        this.id = id;
+
+    }
+
+    public String getCandidateName () {
+
+        return candidateName;
+
+    }
+
+    public Integer getId () {
+
+        return id;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Candidate candidate)) return false;
+        return Objects.equals(getCandidateName(), candidate.getCandidateName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCandidateName());
+    }
+
+    @Override
+    public String toString () {
+
+        StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append( getCandidateName() ).append( ", " ).append( getId() );
+
+        return sb.toString();
+
+    }
+
 }
