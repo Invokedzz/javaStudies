152aa10a5b1a0aef82c22716ab3db1355b4873bf
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-01-20 12:01:52 -0300

    Consumer

--- LambdaJavaDoc/src/Example6th/src/util/ProductPredicate.java
+++ LambdaJavaDoc/src/Example6th/src/util/ProductPredicate.java
@@ -4,9 +4,13 @@ import LambdaJavaDoc.src.Example6th.src.entities.Product;
 
 import java.util.function.Predicate;
 
+/*There are some predefined functional interface in Java like Predicate, consumer, supplier etc.
+ The return type of Lambda function (introduced in JDK 1.8) is also functional interface.*/
+
+@FunctionalInterface
 public interface ProductPredicate extends Predicate <Product> {
 
-    // an interface can't implement another interface, but an interface can extends other interface
+    // an interface can't implement another interface, but an interface can extend other interface
 
     boolean test ( Product product );
 
